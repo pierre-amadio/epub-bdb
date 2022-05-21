9d88f4fee5be763265ec6c0b918c9ee1b85e1f4a
Pierre Amadio <amadio.pierre@gmail.com>
2022-05-21 18:42:18 +0200

    unsure how to parse all that...

--- bin/entry.py
+++ bin/entry.py
@@ -12,5 +12,19 @@ class entry:
   rawsoup=None
 
   def __init__(self, soupEntry):
-    print(soupEntry["id"])
     rawsoup=copy.copy(soupEntry)
+    myid=rawsoup["id"]
+    print("my id",myid)
+    print("--")
+    print(rawsoup.contents)
+    print("@@@@")
+    print(str(rawsoup))
+    
+    if "type" in rawsoup.attrs:
+      mytype=rawsoup["type"]
+    
+    for adef in rawsoup.find_all("def",recursive=False):
+      print(adef)
+
+    for curword in rawsoup.find_all("w",recursive=False):
+      print(curword)
